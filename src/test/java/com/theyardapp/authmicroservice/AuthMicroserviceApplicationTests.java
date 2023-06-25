a708b720b52abc1188ed9a1d42a7bc6cad71f7a7
kap1147 <joshua1147.ad@gmail.com>
2023-06-25 19:45:40 -0400

    add Mongo baseline

---
+++ src/test/java/com/theyardapp/authmicroservice/AuthMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theyardapp.authmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
